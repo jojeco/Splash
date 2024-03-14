cbd4324ed6b4d65c60722261c69c4392de33c0ac
jojeco <145804308+jojeco@users.noreply.github.com>
2024-03-14 00:03:04 -0600

    Works pretty good

--- App.js
+++ App.js
@@ -77,7 +77,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1da1f2',
     alignItems: 'center',
     justifyContent: 'center',
   },
